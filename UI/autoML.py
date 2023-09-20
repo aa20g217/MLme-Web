cda19bd2a455eb2c4542097ded2d0dd5bc09c96b
Akshay Suhag <83011261+aa20g217@users.noreply.github.com>
2023-09-20 17:42:04 +0200

    Update autoML.py for toggle

--- UI/autoML.py
+++ UI/autoML.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Wed Sep 20 17:39:58 2023
+
+@author: akshay
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Sep  9 17:16:54 2021
 
 @author: akshay
@@ -59,10 +67,7 @@ upload_data_sidePanel=dbc.Card([dbc.CardBody([
             
                     ])],className="mt-3",color="dark", outline=True) 
 
-  
-
  
-
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
 inputSidePanel=dbc.Card([dbc.CardBody([ 
@@ -80,21 +85,43 @@ runButton=dbc.Card([dbc.CardBody([
                             html.Div(dbc.Label("Number of Features to Select (%)",style={"margin-top": "10px","font-weight": "bold","font-size": "16px"})),    
                             dbc.Input(type="number",placeholder="in percentage",value=1, id="percentile",min=1,max=100,persistence=True,persistence_type="memory"),
                             
-                            dbc.Checklist(options=[{"label": "Test Set","value": "indepTestSet1"}],
+                            dbc.Row([dbc.Checklist(options=[{"label": "Test Set","value": "indepTestSet1"}],
                                value=[],
                                id="keepTest",
                                inline=True,switch=True,labelStyle={"font-weight": "bold",
                                                                    "font-size": "18px"},
                                labelCheckedStyle={"color": "green"},persistence=True,persistence_type="memory",
-                               style={"margin-top": "10px"}),
-                            html.Div(dbc.Label("It is recommended to keep an independent test set solely for the purpose of testing the model and not for any kind of training.",style={"margin-top": "2px","font-size": "12px",})),
+                               style={"margin-top": "12px"}),
+                            
+                            dbc.Button(html.I( className="fa fa-info-circle fa-lg"),id="indepTestSet1-info-btn",n_clicks=0,style={"margin-left": "30px","margin-top": "15px"}),
+                            dbc.Alert("It is recommended to keep an independent test set solely for the purpose of testing the model and not for any kind of training.",
+                              id="indepTestSet1-info-text",dismissable=True,color="info",is_open=False,)],style={"margin-top": "14px"}),
+                            
+                            
 
+                            dbc.Row([dbc.Checklist(options=[{"label": "Resampling","value": "resampling"}],
+                               value=[],
+                               id="resampling",
+                               inline=True,switch=True,labelStyle={"font-weight": "bold",
+                                                                   "font-size": "18px"},
+                               labelCheckedStyle={"color": "green"},persistence=True,persistence_type="memory",
+                               style={"margin-top": "10px"}),
+                            
+                            dbc.Button(html.I( className="fa fa-info-circle fa-lg"),id="resampling-info-btn",n_clicks=0,style={"margin-top": "15px"}),
+                            dbc.Alert("In machine learning, data resampling involves adjusting the dataset by oversampling the minority class (adding more instances) or undersampling the majority class (removing some instances) to address class imbalance. Toggle it on if you have an imbalanced dataset; we'll apply both oversampling and undersampling methods.",
+                              id="resampling-info-text",dismissable=True,color="info",is_open=False,)],style={"margin-top": "14px"}),
+                            
         
                             html.Div(dbc.Button(html.I("      Run", className="fa fa-solid fa-play-circle-o"),
                             disabled=False,color="primary",id='run_autoML', className="me-1", 
-                            style={"margin-top": "15px","font-weight": "bold","font-size": "18px"}))
+                            style={"margin-top": "20px","margin-left": "100px","font-weight": "bold","font-size": "18px"}))
 
             ])],className="mt-3",color="dark", outline=True)
+
+genrateInfoCallback("resampling")
+genrateCollapseCallback("resampling") 
+genrateInfoCallback("indepTestSet1")
+genrateCollapseCallback("indepTestSet1") 
         
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 # =============================================================================
@@ -266,11 +293,7 @@ resultPanel=dbc.Card([dbc.CardBody([
 # Combine
 # 
 # =============================================================================
-               
-
-
-
-  
+                        
 import dash_loading_spinners as dls
 autoML_content = dbc.Row(
     [   
@@ -290,11 +313,11 @@ autoML_content = dbc.Row(
 
         dbc.Col(dls.Hash(inputSidePanel,color="#FFFFFF"), width=10),
         #dbc.Col(inputSidePanel, width=10),
-                
+
         dbc.Row(html.Div(id="hidden_autoML",style={'display': 'none'})),
 
-    ])
-
+    ]
+)
 
 text_temp=dbc.Card([dbc.CardBody([ 
    
@@ -305,27 +328,7 @@ text_temp=dbc.Card([dbc.CardBody([
 
     ])],className="mt-3",color="dark", outline=True) 
 
-# =============================================================================
-# autoML_content=dbc.Col([
-#                 dbc.Row(dbc.Col(html.Div(text_temp,id="hidden_DemoWarn"))),
-#                 #dbc.Row(dbc.Col(html.Div(id="hidden_Act_automl"),width=12)),
-#     ],width=12)
-# =============================================================================
 
-
-# =============================================================================
-# @app.callback(
-#       Output(component_id='hidden_Act_automl', component_property='children'),
-#       
-#     Input("demoAgg","n_clicks")
-# )
-# def getFilename_autoML(n_clicks): 
-#     if n_clicks!=0:
-#         return autoML_content_actual
-#     
-# =============================================================================
-    
-    
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 # =============================================================================
 # Callbacks
@@ -370,19 +373,19 @@ import shutil
     Input(component_id='autoML_sep', component_property='value'),
     Input('varTH_automl', 'value'),
     Input('percentile', 'value'),
-    Input('keepTest', 'value')]
+    Input('keepTest', 'value'),
+    Input('resampling', 'value')]
        
 )
 
-def runAutoML(n_clicks,sep,varTH_automl,percentile,keepTest): 
+def runAutoML(n_clicks,sep,varTH_automl,percentile,keepTest,resampling): 
     if n_clicks is not None:
         try:
             #read file  
             global inputData,logFolder
             inputData=pd.read_csv(file,index_col=0,sep=sep)
-                        
             X= inputData.iloc[:,0:-1]
-            y = inputData.iloc[:,-1] 
+            y = inputData.iloc[:,-1]
             
             #del all folders from upload loc if there are too much
             filelist = [ f for f in os.listdir("./uploads/") if not f.startswith('.')]
@@ -393,7 +396,6 @@ def runAutoML(n_clicks,sep,varTH_automl,percentile,keepTest):
             #check for NAN values
             if inputData.isnull().values.any():
                 return "Given file contains NaN values. NaN values are not allowed.","",True
-            
             else:
                 return text_temp,"",True
                 #del all folders from autoML_output loc if there are too much
@@ -403,7 +405,7 @@ def runAutoML(n_clicks,sep,varTH_automl,percentile,keepTest):
                         shutil.rmtree(os.path.join("./autoML_output/", f))
                         
                 date = datetime.now().strftime("%I_%M_%S_%p-%d_%m_%Y")
-                runSubscript(inputData,date,varTH_automl,percentile,keepTest)
+                runSubscript(inputData,date,varTH_automl,percentile,keepTest,resampling)
                 
                 #read data
                 logFolder = os.path.join(os.getcwd(),"autoML_output/"+date)
